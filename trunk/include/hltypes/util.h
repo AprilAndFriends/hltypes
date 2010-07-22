b35549dc5ac6402a32e0b7ce4bbb62d0f8d9372e
kspes <kspes@f36d04cb-fc1c-5f49-d033-fe8df6da1487>
2010-07-22 10:55:35 +0000

    common code in util.h

--- trunk/include/hltypes/util.h
+++ trunk/include/hltypes/util.h
@@ -20,4 +20,25 @@ template <class T> T hmax(T a,T b)
 	return (a > b) ? a : b;
 }
 
+template <class T> T sign(T number)
+{
+	if (number >= 0) return 1;
+	else             return -1;
+}
+
+template <class T> T clamp(T value,T min_value,T max_value)
+{
+	if      (value < min_value) value=min_value;
+	else if (value > max_value) value=max_value;
+	return value;
+}
+
+#define dsin(degrees) sin(degrees/57.295779513f)
+#define dcos(degrees) cos(degrees/57.295779513f)
+
+#define foreach(type,lst) for (type* it = lst.iter(); it; it = lst.next())
+#define foreach_reverse(type,lst) for (type* it = lst.riter(); it; it = lst.rnext())
+#define foreach_in_map(type,list) for (std::map<hstr,type>::iterator it=list.begin();it != list.end(); it++)
+#define foreach_in_list(type,lst) for (std::list<type>::iterator it=lst.begin();it != lst.end();it++)
+
 #endif
