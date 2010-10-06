4da20094368df9ea59c5b6d5587078c1390bd1c3
Boris Mikic <boris.blizzard@gmail.com>
2010-10-06 13:37:18 +0000

    - added hdir

--- include/hltypes/hdir.h
+++ include/hltypes/hdir.h
@@ -1,5 +1,5 @@
 /************************************************************************************\
-* This source file is part of the High Level C++ types library                       *
+* This source dir is part of the High Level C++ types library                       *
 * For latest info, see http://libhltypes.sourceforge.net/                            *
 **************************************************************************************
 * Copyright (c) 2010 Kresimir Spes, Boris Mikic, Domagoj Cerjan                      *
@@ -7,35 +7,29 @@
 * This program is free software; you can redistribute it and/or modify it under      *
 * the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php   *
 \************************************************************************************/
-#include "harray.h"
+#ifndef HLTYPES_HDIR_H
+#define HLTYPES_HDIR_H
+
+#include "hltypesExport.h"
+
 #include "hstring.h"
-#include "util.h"
 
-int hrand(int min, int max)
+namespace hltypes
 {
-#ifdef _WIN32
-	return min + rand() * (max - min) / RAND_MAX;
-#else
-	return min + (((double) rand()) / RAND_MAX) * (max - min);
+	class hltypesExport dir
+	{
+	public:
+		dir();
+		~dir();
+		
+		// static
+		
+		static void makedirs(chstr path);
+		
+	};
+}
+
+typedef hltypes::dir hdir;
+
 #endif
-}
-
-int hrand(int max)
-{
-#ifdef _WIN32
-	return rand() * max / RAND_MAX;
-#else
-	return (((double) rand()) / RAND_MAX) * max;
-#endif
-}
-
-float hrandf(float min, float max)
-{
-	return min + rand() * (max - min) / RAND_MAX;
-}
-
-float hrandf(float max)
-{
-	return rand() * max / RAND_MAX;
-}
 
---
+++ src/hdir.cpp
@@ -0,0 +1,35 @@
+#ifdef _WIN32
+#include <direct.h>
+#elif defined(__APPLE__)
+#include <sys/stat.h>
+#define _mkdir(name) mkdir(name, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)
+#endif
+
+#include "hdir.h"
+#include "harray.h"
+
+namespace hltypes
+{
+	dir::dir()
+	{
+	}
+	
+	dir::~dir()
+	{
+	}
+		
+	void dir::makedirs(chstr path)
+	{
+		harray<hstr> folders = path.split("/");
+		if (folders.size() > 0)
+		{
+			hstr path = folders.pop_front();
+			foreach (hstr, it, folders)
+			{
+				path += "/" + (*it);
+				_mkdir(path.c_str());
+			}
+		}
+	}
+	
+}
