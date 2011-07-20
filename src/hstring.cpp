b09eb0bac6ec07f43ffe2ef3985afb518ffcef27
Boris Mikic <boris.blizzard@gmail.com>
2011-07-20 10:16:13 +0000

    - increased hsprintf base buffer from 2 to 256 - increased hsprintf maximum buffer from 256 to 65536 - added exception throwing in case of buffer overflow in hsprintf (only when compiling with _DEBUG)

--- src/hstring.cpp
+++ src/hstring.cpp
@@ -16,6 +16,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "exception.h"
 #include "harray.h"
 #include "hstring.h"
 #include "util.h"
@@ -605,29 +606,39 @@ hstr operator+(char s1, chstr s2)
 
 hstr hvsprintf(const char* format, va_list args)
 {
-	int size = 2;
+	int size = 256; // safe assumption that strings will be usually under 256 characters
 	char* c = new char[size + 1];
 	int count = 0;
-	for (int i = 0; i < 8; i++)
+	int i;
+	for (i = 0; i < 8; i++)
 	{
 		count = vsnprintf(c, size + 1, format, args);
 		if (count == 0)
 		{
+			c[0] = '\0'; // empty string
 			break;
 		}
 		if (count > 0)
 		{
-			delete c;
+			delete [] c;
 			c = new char[count + 1];
 			vsnprintf(c, count + 1, format, args);
+			c[count] = '\0'; // terminate string with \0, safe is safe
 			break;
 		}
-		size *= 2;
-		delete c;
+		size *= 2; // not enough characters, double current buffer
+		delete [] c;
 		c = new char[size + 1];
 	}
+#ifdef _DEBUG
+	if (i == 8)
+	{
+		delete [] c;
+		throw hl_exception("ERROR! Resulting string for hsprintf is longer than 2^16 (65536) characters!");
+	}
+#endif
 	hstr result(c);
-	delete c;
+	delete [] c;
 	return result;
 }
 
