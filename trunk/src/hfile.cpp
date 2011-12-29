91f05cbb83298785e3ed1b03f18985d83e33e737
borisblizzard <borisblizzard@f36d04cb-fc1c-5f49-d033-fe8df6da1487>
2011-12-29 12:43:16 +0000

    - redesigned hfile and split into hfbase and hfile - added prototype hfstream class - bumped version to 1.4.5

--- trunk/src/hfile.cpp
+++ trunk/src/hfile.cpp
@@ -2,7 +2,7 @@
 /// @author  Kresimir Spes
 /// @author  Boris Mikic
 /// @author  Ivan Vucica
-/// @version 1.4
+/// @version 1.5
 /// 
 /// @section LICENSE
 /// 
@@ -10,23 +10,18 @@
 /// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
 
 #include <stdio.h>
-#include <string.h>
-#include <stdarg.h>
 
 // prevents recursive calls of hfile::rename and hfile::remove as these functions are called via these pointers
 int (*f_rename)(const char* old_name, const char* new_name) = rename;
 int (*f_remove)(const char* filename) = remove;
 
 #include "exception.h"
-#include "harray.h"
 #include "hdir.h"
 #include "hfile.h"
-#include "hmap.h"
 #include "hstring.h"
 #include "hthread.h"
-#include "util.h"
 
-#define BUFFER_SIZE (4096)
+#define BUFFER_SIZE 4096
 
 namespace hltypes
 {
@@ -34,17 +29,16 @@ namespace hltypes
 	int File::repeats = 0;
 /******* CONSTRUCT/DESTRUCT ********************************************/
 
-	File::File(chstr filename, AccessMode access_mode, unsigned char encryption_offset) : fileSize(0), cfile(NULL)
+	File::File(chstr filename, AccessMode access_mode, unsigned char encryption_offset) : StreamBase(encryption_offset), cfile(NULL)
 	{
 		this->filename = normalize_path(filename);
 #ifdef NO_FS_TREE
 		this->filename = this->filename.replace("/", "___");
 #endif
-		this->encryption_offset = encryption_offset;
 		this->open(filename, access_mode, encryption_offset);
 	}
 	
-	File::File() : filename(""), fileSize(0), cfile(NULL), encryption_offset(0)
+	File::File() : StreamBase(), filename(""), cfile(NULL)
 	{
 	}
 	
@@ -108,162 +102,46 @@ namespace hltypes
 		};
 		if (this->cfile == NULL)
 		{
-			throw file_not_found(this->filename.c_str());
+			throw file_not_found(this->_descriptor());
 		}
-		this->fileSize = this->_getFileSize();
+		this->_update_data_size();
 	}
 	
-	hstr File::read(chstr delimiter)
+	void File::close()
 	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		hstr result;
-		harray<hstr> parts;
-		int count;
-		int index;
-		while (!this->eof())
-		{
-			char c[BUFFER_SIZE + 1] = {'\0'};
-			count = fread(c, 1, BUFFER_SIZE, this->cfile);
-			if (count == 0)
-			{
-				break;
-			}
-			result += c;
-			if (delimiter != "")
-			{
-				index = result.find(delimiter);
-				if (index >= 0)
-				{
-					this->seek(index - result.size() + delimiter.size(), CURRENT);
-					result = result(0, index);
-					break;
-				}
-			}
-		}
-		result = result.replace("\r", "");
-		return result;
+		this->_check_availability();
+		fclose(this->cfile);
+		this->cfile = NULL;
+		this->data_size = 0;
 	}
 	
-	hstr File::read(int count)
+	hstr File::_descriptor()
 	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		hstr result;
-		int current = BUFFER_SIZE;
-		int read;
-		while (count > 0)
-		{
-			char c[BUFFER_SIZE + 1] = {'\0'};
-			if (count <= BUFFER_SIZE)
-			{
-				current = count;
-			}
-			read = fread(c, 1, current, this->cfile);
-			if (read == 0)
-			{
-				break;
-			}
-			count -= BUFFER_SIZE;
-			result += hstr(c);
-		}
-		return result;
-	}
-
-	hstr File::read_line()
-	{
-		return this->read("\n");
+		return this->filename;
 	}
 	
-	Array<hstr> File::read_lines()
+	long File::_fread(void* buffer, int size, int count)
 	{
-		return this->read().split("\n", -1, false);
+		return fread(buffer, size, count, this->cfile);
 	}
 	
-	void File::write(chstr text)
+	long File::_fwrite(const void* buffer, int size, int count)
 	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		fwrite(text.c_str(), 1, text.size(), this->cfile);
-		this->fileSize = this->_getFileSize();
+		return fwrite(buffer, size, count, this->cfile);
 	}
 	
-	void File::write(const char* text)
+	bool File::_is_open()
 	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		fwrite(text, 1, strlen(text), this->cfile);
-		this->fileSize = this->_getFileSize();
+		return (this->cfile != NULL);
 	}
 	
-	void File::write_line(chstr text)
+	long File::_position()
 	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		fwrite((text + "\n").c_str(), 1, text.size() + 1, this->cfile);
-		this->fileSize = this->_getFileSize();
+		return ftell(this->cfile);
 	}
 	
-	void File::write_line(const char* text)
+	void File::_seek(long offset, SeekMode seek_mode)
 	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		fwrite(text, 1, strlen(text), this->cfile);
-		fwrite("\n", 1, 1, this->cfile);
-		this->fileSize = this->_getFileSize();
-	}
-	
-	void File::writef(const char* format, ...)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		va_list args;
-		va_start(args, format);
-		hstr result(hvsprintf(format, args));
-		va_end(args);
-		this->write(result);
-	}
-
-	int File::read_raw(void* buffer, int count)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		return fread(buffer, 1, count, this->cfile);
-	}
-	
-	int File::write_raw(void* buffer, int count)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		int result = fwrite(buffer, 1, count, this->cfile);
-		this->fileSize = this->_getFileSize();
-		return result;
-	}
-		
-	void File::seek(long offset, SeekMode seek_mode)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
 		int mode = SEEK_CUR;
 		switch (seek_mode)
 		{
@@ -280,461 +158,6 @@ namespace hltypes
 		fseek(this->cfile, offset, mode);
 	}
 	
-	long File::position()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		return ftell(this->cfile);
-	}
-	
-	long File::size()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		return this->fileSize;
-	}
-	
-	bool File::is_open()
-	{
-		return (this->cfile != NULL);
-	}
-	
-	bool File::eof()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		// "feof" doesn't really work if you use a write mode, so we use our own detection
-		return (this->position() >= this->fileSize);
-	}
-	
-	long File::_getFileSize()
-	{
-		long position = this->position();
-		this->seek(0, END);
-		long size = this->position();
-		this->seek(position, START);
-		return size;
-	}
-	
-	void File::close()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		fclose(this->cfile);
-		this->cfile = NULL;
-		this->fileSize = 0;
-	}
-	
-/******* SERIALIZATION DUMP ********************************************/
-
-	void File::dump(char c)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		fwrite(&c, 1, 1, this->cfile);
-	}
-
-	void File::dump(unsigned char c)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		fwrite(&c, 1, 1, this->cfile);
-	}
-
-	void File::dump(int i)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		this->dump((unsigned int)i);
-	}
-
-	void File::dump(unsigned int i)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-#ifndef __BIG_ENDIAN__
-		fwrite(&i, 1, 4, this->cfile);
-#else
-		unsigned char bytes[4] = {0};
-		bytes[3] = (i >> 24) & 0xFF;
-		bytes[2] = (i >> 16) & 0xFF;
-		bytes[1] = (i >> 8) & 0xFF;
-		bytes[0] = i & 0xFF;
-		fwrite(bytes, 1, 4, this->cfile);
-#endif
-	}
-
-	void File::dump(long l)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		this->dump((unsigned long)l);
-	}
-
-	void File::dump(unsigned long l)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-#ifndef __BIG_ENDIAN__
-		if (sizeof(unsigned long) == 8) // long should be 8 bytes
-		{
-			fwrite(&l, 1, 8, this->cfile);
-		}
-		else if (sizeof(unsigned long) == 4) // will work with 4 bytes as well
-		{
-			fwrite(&l, 1, 4, this->cfile);
-			unsigned char bytes[4] = {0};
-			fwrite(bytes, 1, 4, this->cfile);
-		}
-		else // other sizes are unacceptable
-		{
-			throw file_long_error(this->filename.c_str());
-		}
-#else
-		unsigned char bytes[8] = {0};
-		bytes[7] = (l >> 56) & 0xFF;
-		bytes[6] = (l >> 48) & 0xFF;
-		bytes[5] = (l >> 40) & 0xFF;
-		bytes[4] = (l >> 32) & 0xFF;
-		bytes[3] = (l >> 24) & 0xFF;
-		bytes[2] = (l >> 16) & 0xFF;
-		bytes[1] = (l >> 8) & 0xFF;
-		bytes[0] = l & 0xFF;
-		fwrite(bytes, 1, 8, this->cfile);
-#endif
-	}
-
-	void File::dump(short s)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		this->dump((unsigned short)s);
-	}
-
-	void File::dump(unsigned short s)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-#ifndef __BIG_ENDIAN__
-		fwrite(&s, 1, 2, this->cfile);
-#else
-		unsigned char bytes[2] = {0};
-		bytes[1] = (s >> 8) & 0xFF;
-		bytes[0] = s & 0xFF;
-		fwrite(bytes, 1, 2, this->cfile);
-#endif
-	}
-
-	void File::dump(float f)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-#ifndef __BIG_ENDIAN__
-		fwrite((unsigned char*)&f, 1, 4, this->cfile);
-#else
-		// some data voodoo magic
-		this->dump(*((unsigned int*)&f));
-#endif
-	}
-
-	void File::dump(double d)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-#ifndef __BIG_ENDIAN__
-		fwrite((unsigned char*)&d, 1, 8, this->cfile);
-#else
-		// some more data voodoo magic, but this time we make 100% sure it uses 8 bytes
-		int halfSize = sizeof(d) / 2;
-		unsigned int i;
-		memcpy(&i, &d, halfSize);
-		this->dump(i);
-		memcpy(&i, (unsigned char*)&d + halfSize, halfSize);
-		this->dump(i);
-#endif
-	}
-
-	void File::dump(bool b)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		unsigned char c = (b ? 1 : 0);
-		fwrite(&c, 1, 1, this->cfile);
-	}
-
-	void File::dump(chstr str)
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		int size = str.size();
-		this->dump(size);
-		if (size > 0)
-		{
-			if (this->encryption_offset == 0)
-			{
-				fwrite(str.c_str(), 1, size, this->cfile);
-			}
-			else
-			{
-				const char* string = str.c_str();
-				char* c = new char[size];
-				memset(c, 0, size * sizeof(char));
-				for (int i = 0; i < size; i++)
-				{
-					c[i] = string[i] - this->encryption_offset;
-				}
-				fwrite(c, 1, size, this->cfile);
-				delete c;
-			}
-		}
-	}
-
-	void File::dump(const char* c)
-	{
-		this->dump(hstr(c));
-	}
-
-/******* SERIALIZATION LOAD ********************************************/
-
-	char File::load_char()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		char c;
-		fread(&c, 1, 1, this->cfile);
-		return c;
-	}
-
-	unsigned char File::load_uchar()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		unsigned char c;
-		fread(&c, 1, 1, this->cfile);
-		return c;
-	}
-
-	int File::load_int()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		return (int)this->load_uint();
-	}
-
-	unsigned int File::load_uint()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		unsigned int i = 0;
-#ifndef __BIG_ENDIAN__
-		fread((unsigned char*)&i, 1, 4, this->cfile);
-#else
-		unsigned char bytes[4] = {0};
-		fread(bytes, 4, 1, this->cfile);
-		i |= bytes[3] << 24;
-		i |= bytes[2] << 16;
-		i |= bytes[1] << 8;
-		i |= bytes[0];
-#endif
-		return i;
-	}
-
-	long File::load_long()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		return (long)this->load_ulong();
-	}
-
-	unsigned long File::load_ulong()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		unsigned long l = 0;
-#ifndef __BIG_ENDIAN__
-		if (sizeof(unsigned long) == 8) // long should be 8 bytes
-		{
-			fread((unsigned char*)&l, 1, 8, this->cfile);
-		}
-		else if (sizeof(unsigned long) == 4) // will work with 4 bytes as well
-		{
-			fread((unsigned char*)&l, 1, 4, this->cfile);
-			unsigned char bytes[4] = {0};
-			fread(bytes, 1, 4, this->cfile);
-		}
-		else // other sizes are unacceptable
-		{
-			throw file_long_error(this->filename.c_str());
-		}
-#else
-		unsigned char bytes[8] = {0};
-		fread(bytes, 8, 1, this->cfile);
-		l |= bytes[7] << 56;
-		l |= bytes[6] << 48;
-		l |= bytes[5] << 40;
-		l |= bytes[4] << 32;
-		l |= bytes[3] << 24;
-		l |= bytes[2] << 16;
-		l |= bytes[1] << 8;
-		l |= bytes[0];
-#endif
-		return l;
-	}
-
-	short File::load_short()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		return (int)this->load_ushort();
-	}
-
-	unsigned short File::load_ushort()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		unsigned short s = 0;
-#ifndef __BIG_ENDIAN__
-		fread((unsigned char*)&s, 1, 2, this->cfile);
-#else
-		unsigned char bytes[2] = {0};
-		fread(bytes, 1, 2, this->cfile);
-		s |= bytes[1] << 8;
-		s |= bytes[0];
-#endif
-		return s;
-	}
-
-	float File::load_float()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		float f;
-#ifndef __BIG_ENDIAN__
-		fread((unsigned char*)&f, 1, 4, this->cfile);
-#else
-		// data voodoo magic, the float was stored as uint
-		unsigned int i = this->load_uint();
-		f = *((float*)&i);
-#endif
-		return f;
-	}
-
-	double File::load_double()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		double d;
-#ifndef __BIG_ENDIAN__
-		fread((unsigned char*)&d, 1, 8, this->cfile);
-#else
-		// some more data voodoo magic, the double was stored as 2 uints
-		int halfSize = sizeof(d) / 2;
-		unsigned int i = this->load_uint();
-		memcpy((unsigned char*)&d, &i, halfSize);
-		i = this->load_uint();
-		memcpy((unsigned char*)&d + halfSize, &i, halfSize);
-#endif
-		return d;
-	}
-
-	bool File::load_bool()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		unsigned char c;
-		fread(&c, 1, 1, this->cfile);
-		return (c != 0);
-	}
-
-	hstr File::load_hstr()
-	{
-		if (!this->is_open())
-		{
-			throw file_not_open(this->filename.c_str());
-		}
-		int size = this->load_int();
-		hstr str;
-		int count = BUFFER_SIZE;
-		while (size > 0)
-		{
-			char c[BUFFER_SIZE + 1] = {'\0'};
-			if (size < BUFFER_SIZE)
-			{
-				count = size;
-			}
-			fread(c, 1, count, this->cfile);
-			if (this->encryption_offset != 0)
-			{
-				for (int i = 0; i < count; i++)
-				{
-					c[i] += this->encryption_offset;
-				}
-			}
-			size -= BUFFER_SIZE;
-			str += hstr(c);
-		}
-		return str;
-	}
-
-	hstr File::load_string()
-	{
-		return this->load_hstr();
-	}
-
 /******* STATIC ********************************************************/
 
 	bool File::create(chstr filename)
---
+++ trunk/src/hsbase.cpp
@@ -0,0 +1,524 @@
+/// @file
+/// @author  Boris Mikic
+/// @version 1.5
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+
+#include <string.h>
+#include <stdarg.h>
+
+#include "exception.h"
+#include "harray.h"
+#include "hsbase.h"
+#include "hstring.h"
+
+#define BUFFER_SIZE 4096
+
+namespace hltypes
+{
+	StreamBase::StreamBase(unsigned char encryption_offset) : data_size(0)
+	{
+		this->encryption_offset = encryption_offset;
+	}
+	
+	StreamBase::~StreamBase()
+	{
+	}
+	
+	bool StreamBase::is_open()
+	{
+		return this->_is_open();
+	}
+	
+	void StreamBase::seek(long offset, SeekMode seek_mode)
+	{
+		this->_check_availability();
+		this->_seek(offset, seek_mode);
+	}
+	
+	long StreamBase::position()
+	{
+		this->_check_availability();
+		return this->_position();
+	}
+	
+	long StreamBase::size()
+	{
+		this->_check_availability();
+		return this->data_size;
+	}
+	
+	bool StreamBase::eof()
+	{
+		this->_check_availability();
+		// "feof" doesn't really work if you use a write mode, so we use our own detection
+		return (this->_position() >= this->data_size);
+	}
+	
+	hstr StreamBase::read(chstr delimiter)
+	{
+		this->_check_availability();
+		hstr result;
+		harray<hstr> parts;
+		int count;
+		int index;
+		while (!this->eof())
+		{
+			char c[BUFFER_SIZE + 1] = {'\0'};
+			count = this->_fread(c, 1, BUFFER_SIZE);
+			if (count == 0)
+			{
+				break;
+			}
+			result += c;
+			if (delimiter != "")
+			{
+				index = result.find(delimiter);
+				if (index >= 0)
+				{
+					this->seek(index - result.size() + delimiter.size(), CURRENT);
+					result = result(0, index);
+					break;
+				}
+			}
+		}
+		result = result.replace("\r", "");
+		return result;
+	}
+	
+	hstr StreamBase::read(int count)
+	{
+		this->_check_availability();
+		hstr result;
+		int current = BUFFER_SIZE;
+		int read;
+		while (count > 0)
+		{
+			char c[BUFFER_SIZE + 1] = {'\0'};
+			if (count <= BUFFER_SIZE)
+			{
+				current = count;
+			}
+			read = this->_fread(c, 1, current);
+			if (read == 0)
+			{
+				break;
+			}
+			count -= BUFFER_SIZE;
+			result += hstr(c);
+		}
+		return result;
+	}
+
+	hstr StreamBase::read_line()
+	{
+		return this->read("\n");
+	}
+	
+	Array<hstr> StreamBase::read_lines()
+	{
+		return this->read().split("\n", -1, false);
+	}
+	
+	void StreamBase::write(chstr text)
+	{
+		this->_check_availability();
+		this->_fwrite(text.c_str(), 1, text.size());
+		this->_update_data_size();
+	}
+	
+	void StreamBase::write(const char* text)
+	{
+		this->_check_availability();
+		this->_fwrite(text, 1, strlen(text));
+		this->_update_data_size();
+	}
+	
+	void StreamBase::write_line(chstr text)
+	{
+		this->_check_availability();
+		this->_fwrite((text + "\n").c_str(), 1, text.size() + 1);
+		this->_update_data_size();
+	}
+	
+	void StreamBase::write_line(const char* text)
+	{
+		this->_check_availability();
+		this->_fwrite(text, 1, strlen(text));
+		this->_fwrite("\n", 1, 1);
+		this->_update_data_size();
+	}
+	
+	void StreamBase::writef(const char* format, ...)
+	{
+		va_list args;
+		va_start(args, format);
+		hstr result(hvsprintf(format, args));
+		va_end(args);
+		this->write(result);
+	}
+
+	int StreamBase::read_raw(void* buffer, int count)
+	{
+		this->_check_availability();
+		return this->_fread(buffer, 1, count);
+	}
+	
+	int StreamBase::write_raw(void* buffer, int count)
+	{
+		this->_check_availability();
+		int result = this->_fwrite(buffer, 1, count);
+		this->_update_data_size();
+		return result;
+	}
+		
+	void StreamBase::_update_data_size()
+	{
+		long position = this->_position();
+		this->_seek(0, END);
+		this->data_size = this->_position();
+		this->_seek(position, START);
+	}
+
+	void StreamBase::_check_availability()
+	{
+		if (!this->is_open())
+		{
+			throw file_not_open(this->_descriptor());
+		}
+	}
+
+/******* SERIALIZATION DUMP ********************************************/
+
+	void StreamBase::dump(char c)
+	{
+		this->_check_availability();
+		this->_fwrite(&c, 1, 1);
+		this->_update_data_size();
+	}
+
+	void StreamBase::dump(unsigned char c)
+	{
+		this->_check_availability();
+		this->_fwrite(&c, 1, 1);
+		this->_update_data_size();
+	}
+
+	void StreamBase::dump(int i)
+	{
+		this->dump((unsigned int)i);
+	}
+
+	void StreamBase::dump(unsigned int i)
+	{
+		this->_check_availability();
+#ifndef __BIG_ENDIAN__
+		this->_fwrite(&i, 1, 4);
+#else
+		unsigned char bytes[4] = {0};
+		bytes[3] = (i >> 24) & 0xFF;
+		bytes[2] = (i >> 16) & 0xFF;
+		bytes[1] = (i >> 8) & 0xFF;
+		bytes[0] = i & 0xFF;
+		this->_fwrite(bytes, 1, 4);
+#endif
+		this->_update_data_size();
+	}
+
+	void StreamBase::dump(long l)
+	{
+		this->dump((unsigned long)l);
+	}
+
+	void StreamBase::dump(unsigned long l)
+	{
+		this->_check_availability();
+#ifndef __BIG_ENDIAN__
+		if (sizeof(unsigned long) == 8) // long should be 8 bytes
+		{
+			this->_fwrite(&l, 1, 8);
+		}
+		else if (sizeof(unsigned long) == 4) // will work with 4 bytes as well
+		{
+			this->_fwrite(&l, 1, 4);
+			unsigned char bytes[4] = {0};
+			this->_fwrite(bytes, 1, 4);
+		}
+		else // other sizes are unacceptable
+		{
+			throw file_long_error(this->_descriptor());
+		}
+#else
+		unsigned char bytes[8] = {0};
+		bytes[7] = (l >> 56) & 0xFF;
+		bytes[6] = (l >> 48) & 0xFF;
+		bytes[5] = (l >> 40) & 0xFF;
+		bytes[4] = (l >> 32) & 0xFF;
+		bytes[3] = (l >> 24) & 0xFF;
+		bytes[2] = (l >> 16) & 0xFF;
+		bytes[1] = (l >> 8) & 0xFF;
+		bytes[0] = l & 0xFF;
+		this->_fwrite(bytes, 1, 8);
+#endif
+		this->_update_data_size();
+	}
+
+	void StreamBase::dump(short s)
+	{
+		this->dump((unsigned short)s);
+	}
+
+	void StreamBase::dump(unsigned short s)
+	{
+		this->_check_availability();
+#ifndef __BIG_ENDIAN__
+		this->_fwrite(&s, 1, 2);
+#else
+		unsigned char bytes[2] = {0};
+		bytes[1] = (s >> 8) & 0xFF;
+		bytes[0] = s & 0xFF;
+		this->_fwrite(bytes, 1, 2);
+#endif
+		this->_update_data_size();
+	}
+
+	void StreamBase::dump(float f)
+	{
+#ifndef __BIG_ENDIAN__
+		this->_check_availability();
+		this->_fwrite((unsigned char*)&f, 1, 4);
+		this->_update_data_size();
+#else
+		// some data voodoo magic
+		this->dump(*((unsigned int*)&f));
+#endif
+	}
+
+	void StreamBase::dump(double d)
+	{
+#ifndef __BIG_ENDIAN__
+		this->_check_availability();
+		this->_fwrite((unsigned char*)&d, 1, 8);
+		this->_update_data_size();
+#else
+		// some more data voodoo magic, but this time we make 100% sure it uses 8 bytes
+		this->dump(*((unsigned int*)&d));
+		this->dump(*((unsigned int*)&d + sizeof(d) / 2));
+#endif
+	}
+
+	void StreamBase::dump(bool b)
+	{
+		this->_check_availability();
+		unsigned char c = (b ? 1 : 0);
+		this->_fwrite(&c, 1, 1);
+		this->_update_data_size();
+	}
+
+	void StreamBase::dump(chstr str)
+	{
+		this->_check_availability();
+		int size = str.size();
+		this->dump(size);
+		if (size > 0)
+		{
+			if (this->encryption_offset == 0)
+			{
+				this->_fwrite(str.c_str(), 1, size);
+			}
+			else
+			{
+				const char* string = str.c_str();
+				char* c = new char[size];
+				memset(c, 0, size * sizeof(char));
+				for (int i = 0; i < size; i++)
+				{
+					c[i] = string[i] - this->encryption_offset;
+				}
+				this->_fwrite(c, 1, size);
+				delete c;
+			}
+		}
+		this->_update_data_size();
+	}
+
+	void StreamBase::dump(const char* c)
+	{
+		this->dump(hstr(c));
+	}
+
+/******* SERIALIZATION LOAD ********************************************/
+
+	char StreamBase::load_char()
+	{
+		this->_check_availability();
+		char c;
+		this->_fread(&c, 1, 1);
+		return c;
+	}
+
+	unsigned char StreamBase::load_uchar()
+	{
+		this->_check_availability();
+		unsigned char c;
+		this->_fread(&c, 1, 1);
+		return c;
+	}
+
+	int StreamBase::load_int()
+	{
+		return (int)this->load_uint();
+	}
+
+	unsigned int StreamBase::load_uint()
+	{
+		this->_check_availability();
+		unsigned int i = 0;
+#ifndef __BIG_ENDIAN__
+		this->_fread((unsigned char*)&i, 1, 4);
+#else
+		unsigned char bytes[4] = {0};
+		this->_fread(bytes, 4, 1);
+		i |= bytes[3] << 24;
+		i |= bytes[2] << 16;
+		i |= bytes[1] << 8;
+		i |= bytes[0];
+#endif
+		return i;
+	}
+
+	long StreamBase::load_long()
+	{
+		return (long)this->load_ulong();
+	}
+
+	unsigned long StreamBase::load_ulong()
+	{
+		this->_check_availability();
+		unsigned long l = 0;
+#ifndef __BIG_ENDIAN__
+		if (sizeof(unsigned long) == 8) // long should be 8 bytes
+		{
+			this->_fread((unsigned char*)&l, 1, 8);
+		}
+		else if (sizeof(unsigned long) == 4) // will work with 4 bytes as well
+		{
+			this->_fread((unsigned char*)&l, 1, 4);
+			unsigned char bytes[4] = {0};
+			this->_fread(bytes, 1, 4);
+		}
+		else // other sizes are unacceptable
+		{
+			throw file_long_error(this->_descriptor());
+		}
+#else
+		unsigned char bytes[8] = {0};
+		this->_fread(bytes, 8, 1);
+		l |= bytes[7] << 56;
+		l |= bytes[6] << 48;
+		l |= bytes[5] << 40;
+		l |= bytes[4] << 32;
+		l |= bytes[3] << 24;
+		l |= bytes[2] << 16;
+		l |= bytes[1] << 8;
+		l |= bytes[0];
+#endif
+		return l;
+	}
+
+	short StreamBase::load_short()
+	{
+		return (int)this->load_ushort();
+	}
+
+	unsigned short StreamBase::load_ushort()
+	{
+		this->_check_availability();
+		unsigned short s = 0;
+#ifndef __BIG_ENDIAN__
+		this->_fread((unsigned char*)&s, 1, 2);
+#else
+		unsigned char bytes[2] = {0};
+		this->_fread(bytes, 1, 2);
+		s |= bytes[1] << 8;
+		s |= bytes[0];
+#endif
+		return s;
+	}
+
+	float StreamBase::load_float()
+	{
+		float f;
+#ifndef __BIG_ENDIAN__
+		this->_check_availability();
+		this->_fread((unsigned char*)&f, 1, 4);
+#else
+		// data voodoo magic, the float was stored as uint
+		unsigned int i = this->load_uint();
+		f = *((float*)&i);
+#endif
+		return f;
+	}
+
+	double StreamBase::load_double()
+	{
+		double d;
+#ifndef __BIG_ENDIAN__
+		this->_check_availability();
+		this->_fread((unsigned char*)&d, 1, 8);
+#else
+		// some more data voodoo magic, the double was stored as 2 uints
+		int halfSize = sizeof(d) / 2;
+		unsigned int i = this->load_uint();
+		memcpy((unsigned char*)&d, &i, halfSize);
+		i = this->load_uint();
+		memcpy((unsigned char*)&d + halfSize, &i, halfSize);
+#endif
+		return d;
+	}
+
+	bool StreamBase::load_bool()
+	{
+		this->_check_availability();
+		unsigned char c;
+		this->_fread(&c, 1, 1);
+		return (c != 0);
+	}
+
+	hstr StreamBase::load_hstr()
+	{
+		this->_check_availability();
+		int size = this->load_int();
+		hstr str;
+		int count = BUFFER_SIZE;
+		char c[BUFFER_SIZE + 1];
+		while (size > 0)
+		{
+			memset(&c, 0, (BUFFER_SIZE + 1) * sizeof(char));
+			if (size < BUFFER_SIZE)
+			{
+				count = size;
+			}
+			this->_fread(c, 1, count);
+			if (this->encryption_offset != 0)
+			{
+				for (int i = 0; i < count; i++)
+				{
+					c[i] += this->encryption_offset;
+				}
+			}
+			size -= BUFFER_SIZE;
+			str += hstr(c);
+		}
+		return str;
+	}
+
+	hstr StreamBase::load_string()
+	{
+		return this->load_hstr();
+	}
+
+}
