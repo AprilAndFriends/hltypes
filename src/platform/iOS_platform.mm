3b5506b08334fc1f57f5853595da414a41a2907a
kspes <kresimir.spes@gmail.com>
2012-10-23 09:25:21 +0000

    * fixed iOS/Mac logging * added platform files and implemented nsLog for iOS and Mac * copied version info to Info.plist

---
+++ src/platform/iOS_platform.mm
@@ -0,0 +1,17 @@
+/// @file
+/// @author  Boris Mikic
+/// @author  Kresimir Spes
+/// @version 2.0
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
+
+#import <Foundation/Foundation.h>
+#include "hstring.h"
+
+void nsLog(chstr message)
+{
+	NSLog(@"%s", message.c_str());
+}
