8f57b097c6928e21e3de109a27e41437054be899
Boris Mikic <boris.blizzard@gmail.com>
2013-04-29 10:18:48 +0000

    - added static build configuration

--- include/hltypes/hltypesExport.h
+++ include/hltypes/hltypesExport.h
@@ -19,7 +19,7 @@
 	/// @brief Macro for DLL exports/imports.
 	/// @def hltypesFnExport
 	/// @brief Macro for function DLL exports/imports.
-	#ifdef _STATICLIB
+	#ifdef _LIB
 		#define hltypesExport
 		#define hltypesFnExport
 	#else
