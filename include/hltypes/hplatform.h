77f75159cbc278be64e323f8a2e32cfa6b83c8db
Boris Mikic <boris.blizzard@gmail.com>
2013-07-12 13:15:17 +0000

    - WinRT update

--- include/hltypes/hplatform.h
+++ include/hltypes/hplatform.h
@@ -40,7 +40,7 @@
 #include <wrl.h>
 #define _HL_HSTR_TO_PSTR(string) ref new Platform::String((string).w_str().c_str())
 #define _HL_HSTR_TO_PSTR_DEF(string) Platform::String^ p ## string = _HL_HSTR_TO_PSTR(string)
-#define _HL_PSTR_TO_HSTR(string) unicode_to_utf8((string)->Data())
+#define _HL_PSTR_TO_HSTR(string) hstr::from_unicode((string)->Data())
 #define _HL_TRY_DELETE(name) \
 	if (name != NULL) \
 	{ \
