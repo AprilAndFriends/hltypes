660f2c4f14442840233144ccec706e0468208982
boris.blizzard <boris.blizzard@f36d04cb-fc1c-5f49-d033-fe8df6da1487>
2014-05-13 07:46:21 +0000

    - added back the special WinRT exports (required for DLLs that depends on hltypes, but also link to DLLs that include std::string definitions)

--- trunk/include/hltypes/hltypesExport.h
+++ trunk/include/hltypes/hltypesExport.h
@@ -31,6 +31,10 @@
 				#define hltypesExport __declspec(dllimport)
 				#define hltypesFnExport __declspec(dllimport)
 			#endif
+			#ifdef _WINRT
+				#define hltypesSpecialExport
+				#define hltypesMemberExport hltypesExport
+			#endif
 		#else
 			#define hltypesExport __attribute__ ((visibility("default")))
 			#define hltypesFnExport __attribute__ ((visibility("default")))
@@ -42,6 +46,10 @@
 		#else
 			#define DEPRECATED_ATTRIBUTE __attribute__((deprecated))
 		#endif
+	#endif
+	#ifndef hltypesSpecialExport
+		#define hltypesSpecialExport hltypesExport
+		#define hltypesMemberExport
 	#endif
 
 #endif
