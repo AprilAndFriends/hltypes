37f091aac444441610d1820c767b96be736c216f
Boris Mikic <boris.blizzard@gmail.com>
2015-06-11 12:32:03 +0000

    - added HL-Android MSBuild scripts for building

---
+++ msvc/util/hl-android-0.9/data/_install.cmd
@@ -0,0 +1,75 @@
+@echo off
+
+set MsBuildCppDir=
+echo.
+
+rem On 64-bit machines, Visual Studio and MsBuild is in the (x86) directory. So try that last.
+IF EXIST "%ProgramFiles%" set MsBuildCppDir=%ProgramFiles%\MSBuild\Microsoft.Cpp\v4.0\%1\Platforms
+IF EXIST "%ProgramFiles(x86)%" set MsBuildCppDir=%ProgramFiles(x86)%\MSBuild\Microsoft.Cpp\v4.0\%1\Platforms
+
+rem Prepare install.
+IF EXIST "%windir%\system32\hl-install-test" (
+	rmdir "%windir%\system32\hl-install-test"
+)
+
+rem This will fail if we don't have admin privileges.
+mkdir "%windir%\system32\hl-install-test" 2>nul
+IF ERRORLEVEL 1 (
+	
+	echo.This batch file needs to be ran with administrative privileges. Since it copies files to the \Program Files directory.
+	pause
+	Goto cleanup
+
+) ELSE (
+
+	rem Remove that test directory we just made.
+	rmdir "%windir%\system32\hl-install-test"
+
+	IF NOT EXIST "%MsBuildCppDir%" (
+		echo.Failed to find the MsBuild directories that should have been installed with 'Visual Studio'.
+		echo."%MsBuildCppDir%"
+		pause
+		goto cleanup
+	)
+
+	IF NOT EXIST "%MsBuildCppDir%\Android" (
+		IF NOT EXIST "%MsBuildCppDir%\Android-x8" (
+			goto start
+		)
+	)
+	echo.An HL-Android MSBuild toolset already exists.
+	echo.Continuing will delete the version already installed to directory:
+	rd "%MsBuildCppDir%\Android" /s
+	IF EXIST "%MsBuildCppDir%\Android" (
+		goto cleanup
+	)
+	rd "%MsBuildCppDir%\Android-x86" /s /q
+	IF EXIST "%MsBuildCppDir%\Android-x86" (
+		goto cleanup
+	)
+	
+	echo.
+)
+	
+:start
+echo.Installing Android MSBuild files:
+
+md "%MsBuildCppDir%\Android"
+xcopy "data\Android\*.*" "%MsBuildCppDir%\Android" /E
+md "%MsBuildCppDir%\Android-x86"
+xcopy "data\Android-x86\*.*" "%MsBuildCppDir%\Android-x86" /E
+
+IF ERRORLEVEL 1 (
+	echo.Problem with copying
+	pause
+	goto cleanup
+)
+
+echo.
+echo.Done! You will need to close and re-open existing instances of Visual Studio.
+echo.
+pause
+
+
+:cleanup
+set MsBuildCppDir=
