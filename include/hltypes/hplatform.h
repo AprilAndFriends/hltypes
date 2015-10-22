d18796425e3315ac19c6e15ffb9bc228c7aed99c
Boris Mikic <boris.blizzard@gmail.com>
2015-10-22 07:44:27 +0200

    - small update

--- include/hltypes/hplatform.h
+++ include/hltypes/hplatform.h
@@ -38,22 +38,6 @@
 
 #ifndef HLTYPES_PLATFORM_H
 #define HLTYPES_PLATFORM_H
-#if defined(_WIN32) && defined(_MSC_VER)
-// define _WINRT for external projects just in case
-#if !defined(_WINRT) && defined(WINAPI_FAMILY) && defined(WINAPI_FAMILY_PARTITION)
-#if !WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
-#define _WINRT
-#endif
-#endif
-
-#ifdef _WINRT
-#if !defined(_WINP8) && !defined(_OPENKODE)
-#using <Windows.winmd>
-#endif
-#include <wrl.h>
-#define _HL_HSTR_TO_PSTR(string) ref new Platform::String((string).wStr().c_str())
-#define _HL_HSTR_TO_PSTR_DEF(string) Platform::String^ p ## string = _HL_HSTR_TO_PSTR(string)
-#define _HL_PSTR_TO_HSTR(string) hltypes::String::fromUnicode((string)->Data())
 #define _HL_TRY_DELETE(name) \
 	if (name != NULL) \
 	{ \
@@ -72,6 +56,23 @@
 		name->Release(); \
 		name = NULL; \
 	}
+
+#if defined(_WIN32) && defined(_MSC_VER)
+// define _WINRT for external projects just in case
+#if !defined(_WINRT) && defined(WINAPI_FAMILY) && defined(WINAPI_FAMILY_PARTITION)
+#if !WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+#define _WINRT
+#endif
+#endif
+
+#ifdef _WINRT
+#if !defined(_WINP8) && !defined(_OPENKODE)
+#using <Windows.winmd>
+#endif
+#include <wrl.h>
+#define _HL_HSTR_TO_PSTR(string) ref new Platform::String((string).wStr().c_str())
+#define _HL_HSTR_TO_PSTR_DEF(string) Platform::String^ p ## string = _HL_HSTR_TO_PSTR(string)
+#define _HL_PSTR_TO_HSTR(string) hltypes::String::fromUnicode((string)->Data())
 #endif
 #endif
 
