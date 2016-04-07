1ae31491775431fe5986afb15d50269d819214d1
Marko Stanić <mstani19@gmail.com>
2016-04-07 15:17:13 +0200

    - updated unit tests

--- unittest/Deque.cpp
+++ unittest/Deque.cpp
@@ -13,17 +13,17 @@ HL_UT_TEST_CLASS(Deque)
 	{
 		hdeque<int> a;
 		a.add(1);
-		a.add(0);
+		a.addFirst(0);
 		a += 7;
 		a << 20;
-		HL_UT_ASSERT(a[0] == 1, "");
-		//HL_UT_ASSERT(a.front() == 0, ""); //TODO CHECK
-		HL_UT_ASSERT(a[1] == 0, "");
+		HL_UT_ASSERT(a[0] == 0, "");
+		HL_UT_ASSERT(a.first() == 0, "");
+		HL_UT_ASSERT(a[1] == 1, "");
 		HL_UT_ASSERT(a[2] == 7, "");
 		HL_UT_ASSERT(a[3] == 20, "");
-		//HL_UT_ASSERT(a.back() == 20, ""); 
-		HL_UT_ASSERT(a.at(0) == 1, "");
-		HL_UT_ASSERT(a.at(1) == 0, "");
+		HL_UT_ASSERT(a.last() == 20, ""); 
+		HL_UT_ASSERT(a.at(0) == 0, "");
+		HL_UT_ASSERT(a.at(1) == 1, "");
 		HL_UT_ASSERT(a.at(2) == 7, "");
 		HL_UT_ASSERT(a.at(3) == 20, "");
 		HL_UT_ASSERT(a.size() == 4, "");
