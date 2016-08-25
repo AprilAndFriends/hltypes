99d97313483bb5c5715fa22d7e6e1bef58a67fd0
Boris Mikic <boris.blizzard@gmail.com>
2016-08-25 10:07:31 +0200

    - small fix for Container::findFirst() - docs update

--- include/hltypes/hcontainer.h
+++ include/hltypes/hcontainer.h
@@ -825,7 +825,22 @@ namespace hltypes
 		/// @brief Finds and returns first occurrence of element that matches the condition.
 		/// @param[in] conditionFunction Function pointer with condition function that takes one element of type T and returns bool.
 		/// @return Pointer to element that matches the condition or NULL if no element was found.
-		inline T* findFirst(bool (*conditionFunction)(T const&)) const
+		inline T* findFirst(bool(*conditionFunction)(T const&))
+		{
+			int size = this->size();
+			for_iter (i, 0, size)
+			{
+				if (conditionFunction(this->at(i)))
+				{
+					return &this->at(i);
+				}
+			}
+			return NULL;
+		}
+		/// @brief Finds and returns first occurrence of element that matches the condition.
+		/// @param[in] conditionFunction Function pointer with condition function that takes one element of type T and returns bool.
+		/// @return Pointer to element that matches the condition or NULL if no element was found.
+		inline const T* findFirst(bool (*conditionFunction)(T const&)) const
 		{
 			int size = this->size();
 			for_iter (i, 0, size)
