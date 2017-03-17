7593471d82cdd4b0a1f488aaed0a2a0e9e545615
Boris Mikic <boris.blizzard@gmail.com>
2017-03-17 14:49:08 +0100

    - unit test fixes

--- unittest/generated/_Mutex.cpp
+++ unittest/generated/_Mutex.cpp
@@ -7,11 +7,11 @@
 
 HL_UT_TEST_CLASS(Mutex)
 {
-	HL_UT_TEST_FUNCTION(lockrelease);
-	HL_UT_TEST_FUNCTION(mass_lockrelease);
+	HL_UT_TEST_FUNCTION(lockRelease);
+	HL_UT_TEST_FUNCTION(massLockRelease);
 }
 
 HL_UT_RUN_CLASS_BEGIN(Mutex)
-	HL_UT_RUN_METHOD(Mutex, lockrelease)
-	HL_UT_RUN_METHOD(Mutex, mass_lockrelease)
+	HL_UT_RUN_METHOD(Mutex, lockRelease)
+	HL_UT_RUN_METHOD(Mutex, massLockRelease)
 HL_UT_RUN_CLASS_END
