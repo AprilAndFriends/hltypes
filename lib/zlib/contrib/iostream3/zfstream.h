2adcc61706937d8df7c48a8da05a480d806f3a92
Boris Mikic <boris.blizzard@gmail.com>
2017-06-12 14:28:13 +0200

    - updated zlib to v1.2.11

--- lib/zlib/contrib/iostream3/zfstream.h
+++ lib/zlib/contrib/iostream3/zfstream.h
@@ -267,7 +267,7 @@ public:
    *  @return  True if file is open.
   */
   bool
-  is_open() { return sb.isOpen(); }
+  is_open() { return sb.is_open(); }
 
   /**
    *  @brief  Open gzipped file.
@@ -356,7 +356,7 @@ public:
    *  @return  True if file is open.
   */
   bool
-  is_open() { return sb.isOpen(); }
+  is_open() { return sb.is_open(); }
 
   /**
    *  @brief  Open gzipped file.
