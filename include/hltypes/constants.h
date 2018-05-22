5094f1fec0ddffb296f1f9764fafd8b12ea30309
Kresimir Spes <kresimir.spes@gmail.com>
2018-05-22 16:52:54 +0200

    generic constants file

---
+++ include/hltypes/constants.h
@@ -0,0 +1,25 @@
+/// @file
+/// @version 4.0
+/// 
+/// @section LICENSE
+/// 
+/// This program is free software; you can redistribute it and/or modify it under
+/// the terms of the BSD license: http://opensource.org/licenses/BSD-3-Clause
+/// 
+/// @section DESCRIPTION
+/// 
+/// Provides general purpose constants to be used in projects
+
+#ifndef HLTYPES_CONSTANTS_H
+#define HLTYPES_CONSTANTS_H
+
+// ctypes
+#define MAX_INT 0x7FFFFFFF
+// time
+#define MILLISECONDS_IN_SECOND 1000
+// color
+#define MAX_ALPHA 255
+// misc
+#define MAX_PERCENT 100.0f
+
+#endif
