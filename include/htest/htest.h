6b505a630ef940fcadad74bd60fbd2b748b975ba
Boris Mikic <boris.blizzard@gmail.com>
2018-07-02 15:24:47 +0200

    - convention fix

--- include/htest/htest.h
+++ include/htest/htest.h
@@ -22,7 +22,7 @@
 	#define HTEST_LOG(msg) printf("%s\n", hstr(msg).cStr())
 
 	#define HTEST_SUITE_BEGIN \
-		static const char* __assertMsg(chstr msg) { return msg.cStr(); }\
+		static const char* __assertMsg(chstr msg) { return msg.cStr(); } \
 		@interface __EXPAND(_HTEST_CLASS) : XCTestCase \
 		@end \
 		@implementation __EXPAND(_HTEST_CLASS)
@@ -32,32 +32,32 @@
 	#define HTEST_CASE(name) \
 		- (void) test_ ## name \
 		{ \
-			try\
-			{\
-				[self _test_ ## name];\
-			}\
-			catch (hexception& e)\
-			{\
+			try \
+			{ \
+				[self _test_ ## name]; \
+			} \
+			catch (hexception& e) \
+			{ \
 				_XCTFailureHandler(self, YES, __FILE__, __LINE__, @"Unhandled Exception", @"%s", e.getMessage().cStr()); \
-			}\
-		}\
+			} \
+		} \
 		- (void) _test_ ## name
 
 	#define HTEST_CASE_WITH_DATA(name) \
 		- (void) test_ ## name \
 		{ \
-			try\
-			{\
-				hstr dataDir = [[[NSBundle bundleForClass:[self class]] resourcePath] UTF8String];\
-				hstr tempDir = hdir::joinPath([NSTemporaryDirectory() UTF8String], "htest");\
-				hdir::createNew(tempDir);\
-				[self _test_ ## name:dataDir tempDir:tempDir];\
-			}\
-			catch (hexception& e)\
-			{\
+			try \
+			{ \
+				hstr dataDir = [[[NSBundle bundleForClass:[self class]] resourcePath] UTF8String]; \
+				hstr tempDir = hdir::joinPath([NSTemporaryDirectory() UTF8String], "htest"); \
+				hdir::createNew(tempDir); \
+				[self _test_ ## name:dataDir tempDir:tempDir]; \
+			} \
+			catch (hexception& e) \
+			{ \
 				_XCTFailureHandler(self, YES, __FILE__, __LINE__, @"Unhandled Exception", @"%s", e.getMessage().cStr()); \
-			}\
-		}\
+			} \
+		} \
 		- (void) _test_ ## name:(chstr) dataDir tempDir:(chstr) tempDir
 #else
 	#include "CppUnitTest.h"
@@ -72,43 +72,43 @@
 	#define HTEST_SUITE_BEGIN \
 		namespace __EXPAND(_HTEST_LIB) ## __EXPAND(_) ## __EXPAND(_HTEST_CLASS) ## _htest \
 		{ \
-			static wchar_t __assertMessage[2048];\
-			static const wchar_t* __assertMsg(chstr msg) { const char* s = msg.cStr(); int i; for (i = 0; s[i] != 0; ++i) { __assertMessage[i] = s[i]; }; __assertMessage[i] = 0; return __assertMessage; }\
+			static wchar_t __assertMessage[2048]; \
+			static const wchar_t* __assertMsg(chstr msg) { const char* s = msg.cStr(); int i; for (i = 0; s[i] != 0; ++i) { __assertMessage[i] = s[i]; }; __assertMessage[i] = 0; return __assertMessage; } \
 			TEST_CLASS(__EXPAND(_HTEST_CLASS)) \
 			{ \
 			public:
 
 	#define HTEST_SUITE_END };}
 
-	#define HTEST_CASE(name) TEST_METHOD(__EXPAND(_HTEST_LIB) ## __EXPAND(_) ## __EXPAND(_HTEST_CLASS) ## _ ## name)\
-		{\
-			try\
-			{\
+	#define HTEST_CASE(name) TEST_METHOD(__EXPAND(_HTEST_LIB) ## __EXPAND(_) ## __EXPAND(_HTEST_CLASS) ## _ ## name) \
+		{ \
+			try \
+			{ \
 				__EXPAND(_HTEST_LIB) ## __EXPAND(_) ## __EXPAND(_HTEST_CLASS) ## __ ## name(); \
-			}\
-			catch (hexception& e)\
-			{\
-				Assert::Fail(__assertMsg(e.getMessage()));\
-			}\
-		}\
+			} \
+			catch (hexception& e) \
+			{ \
+				Assert::Fail(__assertMsg(e.getMessage())); \
+			} \
+		} \
 		void __EXPAND(_HTEST_LIB) ## __EXPAND(_) ## __EXPAND(_HTEST_CLASS) ## __ ## name()
 
-	#define HTEST_CASE_WITH_DATA(name) TEST_METHOD(__EXPAND(_HTEST_LIB) ## __EXPAND(_) ## __EXPAND(_HTEST_CLASS) ## _ ## name)\
-		{\
-			try\
-			{\
+	#define HTEST_CASE_WITH_DATA(name) TEST_METHOD(__EXPAND(_HTEST_LIB) ## __EXPAND(_) ## __EXPAND(_HTEST_CLASS) ## _ ## name) \
+		{ \
+			try \
+			{ \
 				hstr dataDir = hdir::baseDir(__FILE__); \
-				wchar_t winTempPath[256];\
-				GetTempPathW(256, winTempPath);\
+				wchar_t winTempPath[256]; \
+				GetTempPathW(256, winTempPath); \
 				hstr tempDir = hdir::joinPath(hstr::fromUnicode(winTempPath).replaced("\\", "/"), "htest"); \
 				hdir::createNew(tempDir); \
 				__EXPAND(_HTEST_LIB) ## __EXPAND(_) ## __EXPAND(_HTEST_CLASS) ## __ ## name(dataDir, tempDir); \
-			}\
-			catch (hexception& e)\
-			{\
-				Assert::Fail(__assertMsg(e.getMessage()));\
-			}\
-		}\
+			} \
+			catch (hexception& e) \
+			{ \
+				Assert::Fail(__assertMsg(e.getMessage())); \
+			} \
+		} \
 		void __EXPAND(_HTEST_LIB) ## __EXPAND(_) ## __EXPAND(_HTEST_CLASS) ## __ ## name(chstr dataDir, chstr tempDir)
 	#endif
 #endif
