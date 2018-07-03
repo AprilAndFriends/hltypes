3bc3edcf9e09a61865587f8f2eb1667187b92263
Boris Mikic <boris.blizzard@gmail.com>
2018-07-03 15:28:52 +0200

    - UWP unit test update

---
+++ msvc/vs2015-uwp/htest.py
@@ -0,0 +1,37 @@
+﻿"""
+import os
+import sys
+import subprocess
+
+binPath = sys.argv[1]
+testDlls = {}
+
+for root, dirs, files in os.walk(binPath):
+	for file in files:
+		if file.endswith("_tests.exe") and root.endswith("AppX"):
+			testDlls[file] = os.path.join(root, file)
+
+for name, path in testDlls.items():
+	print "*", name
+	if subprocess.call([path]) != 0:
+		print "UNIT TEST ERROR, aborting further tests"
+		sys.exit(1)
+"""
+import os
+import sys
+import subprocess
+
+VSTEST_PATH = "C:\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\IDE\\CommonExtensions\\Microsoft\\TestWindow\\vstest.console.exe"
+binPath = sys.argv[1]
+testDlls = {}
+
+for root, dirs, files in os.walk(binPath):
+	for file in files:
+		if "AppPackages" in root and not "Dependencies" in root and (file.endswith(".appx") or file.endswith(".appxbundle")):
+			testDlls[file] = os.path.join(root, file)
+
+for name, path in testDlls.items():
+	print "*", name
+	if subprocess.call([VSTEST_PATH, "/Platform:x86", path]) != 0:
+		print "UNIT TEST ERROR, aborting further tests"
+		sys.exit(1)
