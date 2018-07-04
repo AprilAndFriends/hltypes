deadd5f4a3c52f70b02b1830e26b1a96db45c8d5
Boris Mikic <boris.blizzard@gmail.com>
2018-07-04 12:10:44 +0200

    - improved htest.py for UWP to check and install certificate

--- msvc/vs2015-uwp/tests-util/htest.py
+++ msvc/vs2015-uwp/tests-util/htest.py
@@ -4,15 +4,25 @@ import subprocess
 
 VSTEST_PATH = "C:\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\IDE\\CommonExtensions\\Microsoft\\TestWindow\\vstest.console.exe"
 binPath = sys.argv[1]
-testDlls = {}
+testApps = {}
 
 for root, dirs, files in os.walk(binPath):
 	for file in files:
-		if "AppPackages" in root and not "Dependencies" in root and (file.endswith(".appx") or file.endswith(".appxbundle")):
-			testDlls[file] = os.path.join(root, file)
+		if "AppPackages" in root and not "Dependencies" in root and file.endswith(".appx"):
+			testApps[file] = os.path.join(root, file)
 
-for name, path in testDlls.items():
+for name, path in testApps.items():
 	print "*", name
-	if subprocess.call([VSTEST_PATH, "/Platform:x86", path]) != 0:
+	certificateName = path.replace(".appx", ".cer")
+	if not os.path.exists(certificateName):
+		print "CERTIFICATE DOES NOT EXIST, aborting further tests"
+		sys.exit(1)
+	if subprocess.call(["certutil.exe", "-verify", certificateName]) != 0:
+		print "CERTIFICATE INVALID, aborting further tests"
+		sys.exit(1)
+	if subprocess.call(["certutil.exe", "-addstore", "TrustedPeople", certificateName]) != 0:
+		print "CERTIFICATE COULD NOT BE INSTALLED, aborting further tests"
+		sys.exit(1)
+	if subprocess.call([VSTEST_PATH, "/Platform:x86", "/InIsolation", path]) != 0:
 		print "UNIT TEST ERROR, aborting further tests"
 		sys.exit(1)
